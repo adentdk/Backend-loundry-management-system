6aee7d4ace1b949be205107f0ae7962aefa88b6b
aden trisna daud kurnia <adentrisnadaudkurnia@gmail.com>
2020-02-05 05:58:54 +0700

    administrator middleware

---
+++ app/middlewares/administrator.middleware.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+
+const response = require('./../helpers/res');
+
+module.exports = (req, res, next) => {
+  try {
+    const authorization = req.headers.authorization;
+    if (!!authorization) {
+      const token = authorization.split('Bearer ')[1];
+      const user = jwt.verify(token, 'YEAMPLOWW');
+      if (user.role === 'administrator') {
+        return next();
+      }
+      throw new Error('Forbidden'); 
+    }
+    throw new Error('UnAuthenticate');
+  } catch (error) {
+    if (error.message === 'Forbidden') {
+      return response.error(res, {
+        status: 403,
+        message: 'you doesn\'t have access to access this resource'
+      });
+    } else if (error.message === 'UnAuthenticate') {
+      return response.error(res, {
+        status: 401,
+        message: 'no authentication headers'
+      });
+    } else {
+      return response.error(res, {
+        status: 401,
+        message: 'invalid authentication'
+      });
+    }
+  }
+}
