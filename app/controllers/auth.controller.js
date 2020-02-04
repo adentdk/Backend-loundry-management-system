2b174e2b0809abae32a5a04553b001e7ef0a4e4f
aden trisna daud kurnia <adentrisnadaudkurnia@gmail.com>
2020-02-05 05:58:30 +0700

    add auth controller

---
+++ app/controllers/auth.controller.js
@@ -0,0 +1,71 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const response = require('./../helpers/res');
+const validator = require('./../helpers/validator');
+const db = require('./../models');
+
+exports.index = (req, res, next) => {
+  next();
+};
+
+exports.login = (req, res, next) => {
+  const { username, password } = req.body;
+  const errors = [];
+
+  const queryString = `
+  SELECT
+  users.name, 
+  users.username,
+  users.password,
+  roles.name as role
+  FROM users
+  INNER JOIN roles
+  WHERE users.role_id = roles.id
+  AND (users.username = ?)`;
+  const queryParams = [username];
+
+  let user = {};
+  let token = '';
+
+  validator.required({ fieldName: 'username', value: username }, errors);
+  validator.required({ fieldName: 'password', value: password }, errors);
+
+  if (errors.length > 0) {
+    return response.error(res, {
+      message: 'validation error',
+      errors: errors
+    });
+  }
+
+  db.promise().query(queryString, queryParams).then(([rows]) => {
+    if (rows.length === 0) {
+      throw new Error('username not found');
+    }
+    user = rows[0];
+    return bcrypt.compare(password, user.password);
+  }).then(result => {
+    if (!result) {
+      throw new Error('incorrect password');
+    }
+    token = jwt.sign({
+      name: user.name,
+      username: user.username,
+      role: user.role
+    }, 'YEAMPLOWW');
+    return response.ok(res, {
+      message: 'login success',
+      data: {
+        token: token
+      }
+    });
+  }).catch(error => {
+    console.log(error)
+    return response.error(res, {
+      message: 'account not found',
+    });
+  });
+}
+
+exports.registration = (req, res, next) => {
+}
