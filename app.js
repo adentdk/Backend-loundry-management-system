be7307a5d7bae277298bacd47ade968b70440827
aden trisna daud kurnia <adentrisnadaudkurnia@gmail.com>
2020-02-05 06:17:26 +0700

    dotenv.config()

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const dotenv = require('dotenv');
 
 const app = express();
 
 const router = require('./app/routes')
 
+dotenv.config()
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 
 router(app)
 
