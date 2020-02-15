be8a35c232626d2564a4f858f3dc81a1dbfe8f99
aden trisna daud kurnia <adentrisnadaudkurnia@gmail.com>
2020-02-15 19:09:17 +0700

    add inde, members, member/:id

--- app/controllers/administrator.controller.js
+++ app/controllers/administrator.controller.js
@@ -3,7 +3,39 @@ const jwt = require('jsonwebtoken');
 const response = require('./../helpers/res');
 const validator = require('./../helpers/validator');
 const db = require('./../models');
+const member = require('./../models/member.model');
 
-exports.members = (req, res) => {
-  
+exports.index = (req, res)  => {
+  response.ok(res, {
+    data: {
+      text: 'Administrator/index'
+    }
+  })
+}
+
+exports.allMembers = (req, res) => {
+   member.all().then(result => {
+     response.ok(res, {
+       data: result
+     })
+   }).catch(err => {
+     console.log(err)
+     response.error(res, {})
+   })
+}
+
+exports.memberDetails = (req, res) => {
+  const { id } = req.params
+  member.findId(id).then(result => {
+    response.ok(res, {
+      data: result
+    })
+  }).catch(err => {
+    console.log(err)
+    response.error(res, {})
+  })
+}
+
+exports.addMember = (req, res) => {
+  response.ok(res, {})
 }
