d524c8e963048a36ab56ce58793e6e2996c5f7f1
aden trisna daud kurnia <adentrisnadaudkurnia@gmail.com>
2020-02-15 18:43:57 +0700

    add function findId to get member by id

--- app/models/member.model.js
+++ app/models/member.model.js
@@ -10,3 +10,16 @@ exports.all = () => {
     })
   })
 }
+
+exports.findId = (id) => {
+  return new Promise((resolve, reject) => {
+    const queryString = 'SELECT * FROM members WHERE id=?'
+    const queryParams = [id]
+
+    db.promise().query(queryString, queryParams).then(([rows]) => {
+      resolve(rows[0])
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
