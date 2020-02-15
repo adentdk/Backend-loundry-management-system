00376f6598937fee10cc4eaa7f9fc1cf63dfce19
aden trisna daud kurnia <adentrisnadaudkurnia@gmail.com>
2020-02-15 13:51:32 +0700

    update route

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,14 +4,14 @@ const indexRoute = require('./index.route');
 const usersRoute = require('./users.route');
 const authRoute = require('./auth.route');
 
-const administratorMiddleware = require('./../middlewares/administrator.middleware');
+const errorController = require('./../controllers/error.controller');
+
+const isAdmin = require('./../middlewares/administrator.middleware');
 
 module.exports = (app) => {
   app.use('/', indexRoute);
   app.use('/auth', authRoute);
-  
-  app.use(administratorMiddleware);
-  app.use('/users', usersRoute);
+  app.use('/users', isAdmin, usersRoute);
 
   // catch 404 and forward to error handler
   app.use(function(req, res, next) {
@@ -24,10 +24,17 @@ module.exports = (app) => {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    res.status(err.status || 500);
-    res.json({
-      'error': true
-    });
+    if (err.status === 500) {
+      return errorController.serverError(req, res)
+    } else if (err.status === 404) {
+      return errorController.notFound(req, res)
+    } else {
+      res.status(err.status || 500);
+      res.json({
+        'error': true
+      });
+    }
+
   });
 
 };
