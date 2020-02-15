b2ec6da66d8878656247a9615a9f3b413137df6f
aden trisna daud kurnia <adentrisnadaudkurnia@gmail.com>
2020-02-15 19:09:27 +0700

    add inde, members, member/:id

--- app/routes/administrator.route.js
+++ app/routes/administrator.route.js
@@ -4,7 +4,9 @@ var express = require('express');
 var router = express.Router();
 var controller = require('./../controllers/administrator.controller');
 
+router.get('/', controller.index)
 /* Member */
-router.get('/members', controller.members)
+router.get('/members', controller.allMembers)
+router.get('/member/:id', controller.memberDetails)
 
 module.exports = router;
